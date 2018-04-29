7a2f06b47404e331cb2c4f8a6d4ef3ffe5babae8
CSaldanha@panviva.com <CSaldanha@panviva.com>
2018-04-29 14:16:55 +1000

    Initial check-in of module Assignment2

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        //public string Welcome(string name, int id = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        //}
+
+        //public IActionResult Welcome(string name, int numTimes = 1)
+        //{
+        //    ViewData["Message"] = "Hello " + name;
+        //    ViewData["NumTimes"] = numTimes;
+
+        //    return View();
+        //}
+    }
+}
---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public const string SessionKeyMovieID = "_MovieID";
+
+        private readonly MvcMovieContext _context;
+
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        {
+            var genres = _context.Movie.Select(x => x.Genre).Distinct().OrderBy(x => x);
+
+            var movies = _context.Movie.Select(x => x);
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            if(!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genres.ToListAsync()),
+                Movies = await movies.ToListAsync()
+            });
+        }
+
+        // GET: Movies/DetailsAddToSession
+        public async Task<IActionResult> DetailsAddToSession(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            if(!await _context.Movie.AnyAsync(x => x.ID == id))
+            {
+                return NotFound();
+            }
+
+            // Set id into session.
+            HttpContext.Session.SetInt32(SessionKeyMovieID, id.Value);
+
+            return RedirectToAction(nameof(Details));
+        }
+
+        // GET: Movies/Details
+        public async Task<IActionResult> Details()
+        {
+            // Get id from session.
+            var id = HttpContext.Session.GetInt32(SessionKeyMovieID);
+            if(id == null)
+            {
+                return BadRequest();
+            }
+
+            return View(await _context.Movie.SingleAsync(x => x.ID == id));
+        }
+    }
+}
